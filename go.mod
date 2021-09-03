96ebf70b524e188cf562b4dd9f757682f1e6ca7c
洪光华 <guanghua6@staff.weibo.com>
2021-09-03 14:39:55 +0800

    hello_test@v0.1.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ghhong1986/hello_test
+
+go 1.14
+
+require rsc.io/quote/v3 v3.1.0
---
+++ hello.go
@@ -0,0 +1,15 @@
+package hello
+
+import (
+	"rsc.io/quote/v3"
+)
+
+
+// hello
+func Hello() string {
+	return quote.HelloV3()
+}
+
+func Proverb() string {
+	return quote.Concurrency()
+}
